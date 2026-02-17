73a4a82d5c5188699ca930678283ae98f955fe7f
Neema Mwende <gracepete12@gmail.com>
2026-02-17 11:48:14 +0300

    feat: transformers text generation

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+# text summarization
 from transformers import pipeline
+# summerizer = pipeline(task="summarization", model="facebook/bart-large-cnn")
+# text = "Transformers are deep learning models designed to handle sequential data using a mechanism called self-attention. Unlike recurrent networks, they process entire sequences in parallel, making them faster and more scalable. Introduced in the 2017 paper Attention Is All You Need, transformers power modern language models, enabling tasks such as translation, summarization, and code generation with remarkable accuracy across diverse domains."
+# summary= summerizer(text, max_length=150, clean_up_tokenization_spaces=True)
+# print(summary[0]["summary_text"])
 
-summerizer = pipeline(task="summarization", model="facebook/bart-large-cnn")
 
-text = "Transformers are deep learning models designed to handle sequential data using a mechanism called self-attention. Unlike recurrent networks, they process entire sequences in parallel, making them faster and more scalable. Introduced in the 2017 paper Attention Is All You Need, transformers power modern language models, enabling tasks such as translation, summarization, and code generation with remarkable accuracy across diverse domains."
+# text generation
+generator = pipeline(task="text-generation", model="distilgpt2")
+review = "This book was great. I enjoyed the plot twist in chapter 2"
+response = "Dear reader thank you for your review"
+prompt = f"Book review:\n{review}\n\nBook shop response to the review:\n{response}"
+# prompt = "The kenyan government is famous for"
+output = generator(prompt, max_length=100, pad_token_id=generator.tokenizer.eos_token_id, truncation=True)
+print(output[0]["generated_text"])
 
-summary= summerizer(text, max_length=150, clean_up_tokenization_spaces=True)
-print(summary[0]["summary_text"])
+
+
+
