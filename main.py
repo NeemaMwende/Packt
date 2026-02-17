4508ea9fe188d55045de2d2cb50d8c1e2edffdc1
Neema Mwende <gracepete12@gmail.com>
2026-02-17 12:06:36 +0300

    feat: transformers text traslation pipeline

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ from transformers import pipeline
 
 
 # text generation
-generator = pipeline(task="text-generation", model="distilgpt2")
-review = "This book was great. I enjoyed the plot twist in chapter 2"
-response = "Dear reader thank you for your review"
-prompt = f"Book review:\n{review}\n\nBook shop response to the review:\n{response}"
-# prompt = "The kenyan government is famous for"
-output = generator(prompt, max_length=100, pad_token_id=generator.tokenizer.eos_token_id, truncation=True)
-print(output[0]["generated_text"])
+# generator = pipeline(task="text-generation", model="distilgpt2")
+# review = "This book was great. I enjoyed the plot twist in chapter 2"
+# response = "Dear reader thank you for your review"
+# prompt = f"Book review:\n{review}\n\nBook shop response to the review:\n{response}"
+# # prompt = "The kenyan government is famous for"
+# output = generator(prompt, max_length=100, pad_token_id=generator.tokenizer.eos_token_id, truncation=True)
+# print(output[0]["generated_text"])
 
 
-
+#text translation
+translator = pipeline(task="translation_en_to_es", model="Helsinki-NLP/opus-mt-en-es")
+text = "I dont know how i am going to do it yet but i know i will get there"
+output = translator(text, clean_up_tokenization_spaces=True)
+print(output[0]["translation_text"])
 
